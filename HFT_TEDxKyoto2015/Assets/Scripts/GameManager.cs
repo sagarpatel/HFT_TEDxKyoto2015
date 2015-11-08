7fa74de40a6bfeb6141e90b8ff079d4b1aaee42b
Sagar Patel <17.sagar.patel@gmail.com>
2015-11-08 09:42:33 +0900

    Implemented top scores for 3 players, tested works, also have reset on esc key

--- HFT_TEDxKyoto2015/Assets/Scripts/GameManager.cs
+++ HFT_TEDxKyoto2015/Assets/Scripts/GameManager.cs
@@ -8,6 +8,13 @@ public class GameManager : MonoBehaviour
 {
 	public Text playerText_1;
 	public RawImage playerColor_1;
+	
+	public Text playerText_2;
+	public RawImage playerColor_2;
+
+	public Text playerText_3;
+	public RawImage playerColor_3;
+
 
 
 	public void RefreshTopScores()
@@ -16,10 +23,32 @@ public class GameManager : MonoBehaviour
 		playersData = playersData.OrderByDescending(playerData => playerData.m_playerScore).ToArray();
 
 		playerText_1.text = playersData[0].m_text.text;
-		playerColor_1.color = playersData[0].m_rawImage.color;
+		playerColor_1.color = playersData[0].m_rawImage.material.color;
+
+		if(playersData[1] != null)
+		{
+			playerText_2.text = playersData[1].m_text.text;
+			playerColor_2.color = playersData[1].m_rawImage.material.color;
+		}
+
+		
+		if(playersData[2] != null)
+		{
+			playerText_3.text = playersData[2].m_text.text;
+			playerColor_3.color = playersData[2].m_rawImage.material.color;
+		}
 
 	}
 
+
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			Application.LoadLevel(Application.loadedLevel);
+		}
+
+	}
 
 
 }
